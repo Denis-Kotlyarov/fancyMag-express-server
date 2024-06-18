160ab4b42853e9c07194292eeb1824d19f5d6a4a
Denis <156504836+Denis-Kotlyarov@users.noreply.github.com>
2024-06-18 23:13:41 +0300

    Add swagger DOC

--- app.js
+++ app.js
@@ -1,10 +1,34 @@
+// Начальные импорты
 var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+// Импорт моделей, для ожидания их синхронизации при старте сервера
 const { init } = require("./models/init");
 
+// Импорты для документации
+const swaggerDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+const options = {
+  definition: {
+    openapi: "3.0.2",
+    info: {
+      title: "FancyMag",
+      description: "Базовоя документация API нашего сервиса, тестовая сборка",
+      version: "1.0.0",
+    },
+    servers: [
+      {
+        url: "http://localhost:3000/",
+        description: "Local server"
+      }
+    ]
+  },
+  apis: ["./routes/*.js", "./controllers/*.js", "./services/*.js", "./models/*.js"],
+}
+
+// Импорт роутов
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 
@@ -14,14 +38,17 @@ var app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
+// Доп. настройки
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+// Подключение роутов + дока
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc(options))) //!Дока
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -10,3 +10,221 @@ router.put("/:id", userController.updateUser);
 router.delete("/:id", userController.deleteUser);
 
 module.exports = router;
+
+//!Схема
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *      User:
+ *          type: object
+ *          required:
+ *              - first_name
+ *              - last_name
+ *              - email   
+ *              - password
+ *          properties:
+ *              id:
+ *                  type: number
+ *                  primaryKey: true
+ *                  autoIncrement: true
+ *                  description: Автогенерируемое ID
+ *              first_name:
+ *                  type: string
+ *                  description: Имя пользователя
+ *              last_name:
+ *                  type: string
+ *                  description: Фамилия пользователя
+ *              email:
+ *                  type: string
+ *                  unique: true
+ *                  description: Email пользователя
+ *              password:
+ *                  type: string
+ *                  description: Пароль пользователя (в хеше)
+*/
+
+//!Все пользователи + БАЗОВОЕ ОПИСАНИЕ
+/**
+ * @swagger
+ * tags:
+ *  name: Users
+ *  description: Базавое API для модели пользователей
+ * /users:
+ *  get:
+ *      summary: Получениe списка всех пользователей
+ *      tags: [Users]
+ *      responses:
+ *          200:
+ *              description: Удачное получение
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          properties:
+ *                              status:
+ *                                  type: number
+ *                              data:
+ *                                  type: array
+ *                                  items:
+ *                                      $ref: "#/components/schemas/User"
+ *          400:
+ *              description: Что-то пошло не так
+*/
+
+//!Пользователь по ID
+/**
+ * @swagger
+ * tags:
+ *  name: Users
+ * /users/{id}:
+ *  get:
+ *      summary: Получить пользователя по ID
+ *      parameters:
+ *        - name: id
+ *          in: path
+ *          required: true
+ *          schema:
+ *              type: integer
+ *              format: int64
+ *              minimum: 1
+ *          description: Уникальный идентификатор пользователя.
+ *      tags: [Users]
+ *      responses:
+ *          200:
+ *              description: Удачное получение
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          properties:
+ *                              status:
+ *                                  type: number
+ *                              data:
+ *                                  $ref: "#/components/schemas/User"
+ *          404:
+ *              description: Пользователь не найден
+*/
+
+//!Создать пользователя
+/**
+ * @swagger
+ * tags:
+ *  name: Users
+ * /users:
+ *  post:
+ *      summary: Создать пользователя
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      type: object
+ *                      properties:
+ *                          first_name:
+ *                              type: string
+ *                          last_name:
+ *                              type: string
+ *                          email:
+ *                              type: string
+ *                          password:
+ *                              type: string
+ *      tags: [Users]
+ *      responses:
+ *          201:
+ *              description: Пользователь создан
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          properties:
+ *                              status:
+ *                                  type: number
+ *                              data:
+ *                                  $ref: '#/components/schemas/User'
+ *          400:
+ *              description: Неправильный запрос
+*/
+
+//!Обновить пользователя
+/**
+ * @swagger
+ * tags:
+ *  name: Users
+ * /users/{id}:
+ *  put:
+ *      summary: Обновить пользователя
+ *      parameters:
+ *        - name: id
+ *          in: path
+ *          required: true
+ *          schema:
+ *              type: integer
+ *              format: int64
+ *              minimum: 1
+ *          description: Уникальный идентификатор пользователя.
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      type: object
+ *                      properties:
+ *                          first_name:
+ *                              type: string
+ *                          last_name:
+ *                              type: string
+ *                          email:
+ *                              type: string
+ *                          password:
+ *                              type: string
+ *      tags: [Users]
+ *      responses:
+ *          201:
+ *              description: Пользователь обновлен
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          properties:
+ *                              status:
+ *                                  type: number
+ *                              data:
+ *                                  type: String
+ *          400:
+ *              description: Неправильный запрос
+*/
+
+//!Удалить пользователя
+/**
+ * @swagger
+ * tags:
+ *  name: Users
+ * /users/{id}:
+ *  delete:
+ *      summary: Удалить пользователя
+ *      parameters:
+ *        - name: id
+ *          in: path
+ *          required: true
+ *          schema:
+ *              type: integer
+ *              format: int64
+ *              minimum: 1
+ *          description: Уникальный идентификатор пользователя.
+ *      tags: [Users]
+ *      responses:
+ *          201:
+ *              description: Пользователь удален
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          properties:
+ *                              status:
+ *                                  type: number
+ *                              data:
+ *                                  type: String
+ *          404:
+ *              description: Пользователь не найден
+*/
